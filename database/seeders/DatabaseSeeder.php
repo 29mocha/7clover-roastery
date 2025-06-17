4c6f3de366f31907eb5b469eca1191bcf64ec605
29mocha <fireflies1717@gmail.com>
2025-06-17 12:50:00 +0700

    Update Perubahan Final

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,20 +18,23 @@ class DatabaseSeeder extends Seeder
         $this->call([
             SettingSeeder::class,
         ]);
-
-        // Hapus atau beri comment pada user factory yang lama
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
         
-        // Buat user admin utama dengan data yang Anda minta
+        // Buat user admin utama
         User::create([
             'name' => 'RANI',
             'email' => 'rani.ariana@gmail.com',
             'email_verified_at' => now(), // Verifikasi email secara otomatis
             'password' => Hash::make('RANI123456'), // Menggunakan Hash::make() untuk enkripsi
             'role' => 'admin', // Menetapkan peran sebagai admin
+        ]);
+
+        // Buat user roaster
+        User::create([
+            'name' => 'Tony',
+            'email' => 'tony@gmail.com',
+            'email_verified_at' => now(), // Verifikasi email secara otomatis
+            'password' => Hash::make('tony123'), // Menggunakan Hash::make() untuk enkripsi
+            'role' => 'roaster', // Menetapkan peran sebagai roaster
         ]);
     }
 }
